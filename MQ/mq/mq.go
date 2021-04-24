59e202d5f4146e509b11c81a320422e0f267e4f4
impact-eintr <yixingwei4@gmail.com>
2021-04-24 17:39:15 +0800

    MQ

--- MQ/mq/mq.go
+++ MQ/mq/mq.go
@@ -1,4 +1,6 @@
-package internal
+package mq
+
+import "sync"
 
 type Broker interface {
 	// 消息推送 主题 消息
@@ -13,4 +15,12 @@ type Broker interface {
 	// 服务端广播消息
 	broadcast(msg interface{}, subscribers []chan interface{})
 	setCondtions(capacity int)
+}
+
+type BrokerImpl struct {
+	exit         chan bool
+	capacity     int
+	topics       map[string][]chan interface{} // key： topic  value ： queue
+	sync.RWMutex                               // 同步锁
+
 }
