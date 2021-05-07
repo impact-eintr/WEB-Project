5ad9ba572cb8770c1fc7c77e6a3334bf9cb36d0d
impact-eintr <yixingwei4@gmail.com>
2021-05-07 22:16:44 +0800

    Zinx6

---
+++ Zinx/ziface/IMsgHandler.go
@@ -0,0 +1,9 @@
+package ziface
+
+// 消息管理抽象层
+type IMsgHandle interface {
+	// 调度/执行对应的Router消息处理方法
+	DoMsgHandler(IRequest)
+	// 为消息添加具体的处理逻辑
+	AddRouter(uint32, IRouter)
+}
---
+++ Zinx/znet/msgHandler.go
@@ -0,0 +1,36 @@
+package znet
+
+import (
+	"Zinx/ziface"
+	"fmt"
+	"log"
+)
+
+// 消息管理实现
+type MsgHandle struct {
+	// 存放每个MsgID所对应的处理方法(之后是否可以编写路由组)
+	Apis map[uint32]ziface.IRouter
+}
+
+func NewMsgHandle() *MsgHandle {
+	return &MsgHandle{
+		Apis: make(map[uint32]ziface.IRouter),
+	}
+}
+
+// 调度/执行对应的Router消息处理方法
+func (h *MsgHandle) DoMsgHandler(request ziface.IRequest) {
+
+}
+
+// 为消息添加具体的处理逻辑
+func (h *MsgHandle) AddRouter(msgId uint32, router ziface.IRouter) {
+	if _, ok := h.Apis[msgId]; ok {
+		log.Println("repeat api, migId = ", msgId)
+		return
+	}
+
+	h.Apis[msgId] = router
+	fmt.Println("Add Router [MsgId] Succ")
+
+}
